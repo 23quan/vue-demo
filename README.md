86fa1bc3d31753fda6170e34895b899518e40722
lizhiquan <3025073273@qq.com>
2023-08-17 16:17:55 +0800

    提交项目

---
+++ README.md
@@ -0,0 +1,10 @@
+```bash
+# 项目创建
+vue create xxx
+
+# 安装依赖
+npm install / cnpm install
+
+# 启动服务
+npm run dev / npm run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+
+    <p v-if="show">true显示文字</p>
+
+    <div v-if="Math.random() > 1">随机数大于1</div>
+
+    <div v-else>随机数小于1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "vue展示",
+      show: true,
+      namelist: [
+        { text: "斯内克" },
+        { text: "爱丽丝" },
+        { text: "张大炮" }
+      ]
+    }
+  }
+}
+</script>
